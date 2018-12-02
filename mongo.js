751c3bc4086934bf49cea353b4549319ed1a8715
Reetu Kontio <reetu.kontio@aalto.fi>
2018-12-03 01:44:57 +0200

    3.12

---
+++ mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const url = 'mongodb://stuff@ds251902.mlab.com:51902/persons'
+
+mongoose.connect(url)
+
+const Person = mongoose.model('Person', {
+    id: Number,
+    name: String,
+    number: String
+})
+
+if (process.argv.length > 2) {
+    const person = new Person({
+        id: Math.floor(Math.random() * 99999),
+        name: process.argv[2],
+        number: process.argv[3]
+    })
+    person
+        .save()
+        .then(response => {
+            console.log('person saved!')
+            mongoose.connection.close()
+        })
+}
+else {
+    Person
+        .find({})
+        .then(result => {
+            result.forEach(person => {
+                console.log(`${person.name} ${person.number}`)
+            })
+            mongoose.connection.close()
+        })
+}
