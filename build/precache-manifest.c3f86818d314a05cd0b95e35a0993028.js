476b37afc54005d3fd2d726ebf74a36fd01c8f8e
Reetu Kontio <reetu.kontio@aalto.fi>
2018-12-02 18:19:43 +0200

    3.11 wip

---
+++ build/precache-manifest.c3f86818d314a05cd0b95e35a0993028.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9df6dca5c837805e5f71",
+    "url": "/static/js/main.9df6dca5.chunk.js"
+  },
+  {
+    "revision": "d26157ae0134cdb5d99c",
+    "url": "/static/js/1.d26157ae.chunk.js"
+  },
+  {
+    "revision": "9df6dca5c837805e5f71",
+    "url": "/static/css/main.53d62f1b.chunk.css"
+  },
+  {
+    "revision": "92c9f6bab5e53a43ae1251e64419393e",
+    "url": "/index.html"
+  }
+];
