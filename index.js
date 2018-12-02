4f984cf8f22fad497268a6876e7f81a67e4ab584
Reetu Kontio <reetu.kontio@aalto.fi>
2018-12-02 18:08:27 +0200

    3.11 wip

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static('build'))
 
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 
